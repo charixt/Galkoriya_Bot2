6a211a9ef358ad7cf4f9ea1bc62f33de73808ca5
chari997 <93028870+chari997@users.noreply.github.com>
2022-08-20 18:59:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/TeamAlphaTg/MemehubtgSl_Bot.git /MemehubtgSl_Bot
-cd /MemehubtgSl_Bot
+git clone https://github.com/chari997/got.git /got
+cd /got
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
