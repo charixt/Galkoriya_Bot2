ca98b8f4ac9f69928d372ee3f41604f1ee646c9b
charixt <90853856+charixt@users.noreply.github.com>
2022-08-22 22:27:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/chari997/got.git /got
+git clone https://github.com/charixt/Galkoriya_Bot2.git /got
 cd /got
 pip3 install -r requirements.txt
 echo "Starting Bot...."
