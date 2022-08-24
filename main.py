b6cb17ebc68b00a1ddd5b1f838240d849af2c2da
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-08-24 16:04:37 +0530

    `Galkoriya_Bot2`

--- main.py
+++ main.py
@@ -126,7 +126,16 @@ async def startprivate(client, message):
         disable_web_page_preview=True,
         quote=True
     )
-    
+
+
+@Client.on_message(filters.command("ping") )
+async def _ping(bot, update):
+    start = datetime.now()
+    replied = await update.reply('`procesing....`')
+    end = datetime.now()
+    m_s = (end - start).microseconds / 1000
+    await replied.edit(f"**Pong!**\n`{m_s} ms`")
+
 @Client.on_message(filters.incoming & filters.chat(-1001645439750))
 async def bchanl(bot, update, broadcast_ids={}): 
     all_users = await db.get_all_users()
